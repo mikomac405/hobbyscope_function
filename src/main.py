c46dba9729e834b638a5d77d99a65a612cb79f75
mikomac405 <mikolajmacura0510@gmail.com>
2024-11-30 23:02:57 +0100

    fix log

--- src/main.py
+++ src/main.py
@@ -28,10 +28,10 @@ def main(context):
             queries=[Query.equal('user_id',[user_id])],
         )
 
-        if len(answer['count']) == 0:
-            raise ValueError(f"Answer not found for user {user_id}!")
-        elif len(answer['count']) > 1:
-            raise ValueError(f"Too much answers found for user {user_id}! Investigate database!")
+        #if len(answer['count']) == 0:
+        #    raise ValueError(f"Answer not found for user {user_id}!")
+        #elif len(answer['count']) > 1:
+        #    raise ValueError(f"Too much answers found for user {user_id}! Investigate database!")
 
         hobbies_res = databases.list_documents(
             database_id=os.environ["APPWRITE_DATABASE_ID"],
